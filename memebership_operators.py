78e25327b0eff3b45e5fdd1c2d69c39ecc337b3b
Hridya Suresh <hridyasuresh766@gmail.com>
2025-07-07 13:36:51 +0530

    modified membership_operators.py

--- memebership_operators.py
+++ memebership_operators.py
@@ -1,4 +1,4 @@
 a=['hridya',23,2.3,'cat']
-print('hridya' in a)
+print('hridya' in a) #return true if element present in the list
 
-print(23 not in a)
+print(23 not in a) #return true if the element not present in list
--- sample.py
+++ sample.py
@@ -12,4 +12,4 @@ else:
 num1=int(input("Enter first number:"))
 num2=int(input("Enter second number:"))
 result=num1+num2
-print(num)
+print(result)
