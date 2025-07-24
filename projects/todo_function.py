e1d24bcb2f179d7dcde0aaacb3ab0301bd78c946
Hridya Suresh <hridyasuresh766@gmail.com>
2025-07-24 12:50:46 +0530

    Last commit of To-Do project using functons

--- projects/todo_function.py
+++ projects/todo_function.py
@@ -1,9 +1,8 @@
-
 todo_list=[]
 
 def AddTask():
-    id=int(input("How many task you want to add :"))
-    for i in range(id):
+    task_id=int(input("How many task you want to add :"))
+    for i in range(task_id):
         task=input("Enter the task:")
         todo_list.append([task,False])
         print("Task added successfully.") 
@@ -12,10 +11,10 @@ def AddTask():
 
 def UpdateTask():
     try:
-        id=int(input("Enter task id to update:"))
-        todo_list[id-1][1]=not todo_list[id-1][1]
-        stat="completed" if todo_list[id-1][1] else "Not completed"
-        print(f"Task {id} is updated as {stat}")
+        task_id=int(input("Enter task id to update:"))
+        todo_list[task_id-1][1]=not todo_list[task_id-1][1]
+        stat="completed" if todo_list[task_id-1][1] else "Not completed"
+        print(f"Task {task_id} is updated as {stat}")
     except Exception as e:
         print(e)
         print("Failed to update task")
@@ -23,9 +22,9 @@ def UpdateTask():
     
 def RemoveTask():
     try:
-        id=int(input("Enter task id to be removed:"))
-        todo_list.pop(id-1)
-        print(f"Task {id} has removed.")
+        task_id=int(input("Enter task id to be removed:"))
+        todo_list.pop(task_id-1)
+        print(f"Task {task_id} has removed.")
     except Exception as e:
         print(e)
         print("Failed to remove task")
@@ -36,17 +35,19 @@ def ShowTask():
     if not todo_list:
         print("No tasks")
     else:
-        print("\n==Tasks==")
-        for id,(task,status) in enumerate(todo_list,1):       
-            print(f"{id}. {task} - {"Completed" if status else "Not completed"}")
+        #print("\n=====Tasks=====")
+        for task_id,(task,status) in enumerate(todo_list,1):       
+            print(f"{task_id}. {task} - {'Completed' if status else 'Not completed'}")
 
 
 while True:
     print("\n=====To-Do List======")
     print("1.Add Tasks\n2.Show Tasks\n3.Update task\n4.Remove Tasks\n5.Exit\n")
     
+    
     ch=int(input("Enter your choice:"))
     
+        
     match ch:
         case 1:
             AddTask()
@@ -57,5 +58,7 @@ while True:
         case 4:
             RemoveTask()
         case 5:
+            print("Program Exited..!!")
             break
-        
+        case _:
+            print("Invalid choice. Please enter a choice between 1-5")
